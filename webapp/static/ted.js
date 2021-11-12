1233941f81eaf95cb647e5937b523250aec1d3f2
Carl Tankersley <tankersleyc@carleton.edu>
2021-11-11 21:27:33 -0600

    added ted.js

---
+++ webapp/static/ted.js
@@ -0,0 +1,41 @@
+/* Soren DeHaan and Carl Tankersley
+ * 11 November 2021
+ * CS 257 - Software Design
+ */
+
+window.onload = initialize;
+
+function initialize(){
+    let element = document.getElementById('random_speaker_button');
+    if(element){
+        element.onclick = onRandomSpeakerButton;
+    }
+}
+
+function getAPIBaseURL(){
+    let baseURL = window.location.protocol
+                    + '//' + window.location.hostname 
+                    + ':' + window.location.port
+                    + '/api';
+    return baseURL;
+}
+
+function onRandomSpeakerButton(){
+    let url = getAPIBaseURL() + '/random/';
+
+    fetch(url, {method: 'get'})
+    .then((response) => response.json())
+    .then(function(speaker){
+        let str = '';
+        str += '<h2>' + speaker['name']
+             + ', ' + speaker['title']
+             + '</h2>';
+        let newHTML = document.getElementById('random_speaker');
+        if(newHTML){
+            newHTML.innerHTML = str;
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+}
