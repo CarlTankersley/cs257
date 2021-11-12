d39b0ef5281e7648f600d81c7bfa60b7ac6e187c
Soren DeHaan <dehaans@carleton.edu>
2021-11-11 21:16:20 -0600

    random speaker endpoint

--- webapp/api.py
+++ webapp/api.py
@@ -4,19 +4,35 @@
 
 import psycopg2
 import flask
+import random
+import json
 from config import database, user, password
 
 api = flask.Blueprint('api', __name__)
 
+
 def setup_db():
     try:
-        connection = psycopg2.connect(database=database, user=user, password=password)
+        connection = psycopg2.connect(
+            database=database, user=user, password=password)
         cursor = connection.cursor()
     except Exception as e:
         print(e)
         exit()
     return connection, cursor
 
-@api.route('/about')
-def get_about():
-    return flask.render_template('about.html')
+
+@api.route('/random/')
+def get_random_speaker():
+    rand_int = random.randint(0, 100)
+    query = '''SELECT main_speaker, title
+               FROM event_speaker_talk
+               WHERE id = %s'''
+    cursor, connection = setup_db()
+    cursor.execute(query, (rand_int,))
+    speaker = {}
+    for row in cursor:
+        speaker = {'name': row[0], 'title': row[1]}
+    cursor.close()
+    connection.close()
+    return json.dumps(speaker)
