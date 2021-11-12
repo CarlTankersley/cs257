fe9f4875e28823a05a09b870b5faaf9a206aa3aa
Carl Tankersley <tankersleyc@carleton.edu>
2021-11-11 20:20:32 -0600

    committed so I can pull

--- webapp/app.py
+++ webapp/app.py
@@ -3,6 +3,8 @@ import json
 import psycopg2
 import argparse
 
+app = flask.Flask(__name__, static_folder=static, template_folder=templates)
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("host", type=str, help="host on which to run the service")
@@ -13,7 +15,10 @@ def parse_args():
 def main():
     args = parse_args()
     app.run(host=args.host, port=args.port, debug=True)
+
+@app.route('/')
+def home():
+    return flask.render_template('index.html')
     
 if __name__ == '__main__':
-    app = flask.Flask(__name__)
     main()
