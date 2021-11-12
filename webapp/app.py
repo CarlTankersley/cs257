9daa5db767e86b6da7f8793d4a06d9c31086d16d
Soren DeHaan <dehaans@carleton.edu>
2021-11-11 21:03:37 -0600

    now we have a website

--- webapp/app.py
+++ webapp/app.py
@@ -8,23 +8,60 @@ import psycopg2
 import argparse
 import api
 
-app = flask.Flask(__name__, static_folder='static', template_folder='templates')
+app = flask.Flask(__name__, static_folder='static',
+                  template_folder='templates')
 app.register_blueprint(api.api, url_prefix='/api')
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("host", type=str, help="host on which to run the service")
-    parser.add_argument("port", type=int, help="port on which the service is listening")
+    parser.add_argument(
+        "host", type=str, help="host on which to run the service")
+    parser.add_argument(
+        "port", type=int, help="port on which the service is listening")
     args = parser.parse_args()
     return args
+
 
 def main():
     args = parse_args()
     app.run(host=args.host, port=args.port, debug=True)
 
+
 @app.route('/')
 def home():
     return flask.render_template('index.html')
-    
+
+
+@app.route('/about.html')
+def about():
+    return flask.render_template('about.html')
+
+
+@app.route('/careers.html')
+def careers():
+    return flask.render_template('careers.html')
+
+
+@app.route('/create.html')
+def create():
+    return flask.render_template('create.html')
+
+
+@app.route('/index.html')
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/video.html')
+def video():
+    return flask.render_template('video.html')
+
+
+@app.route('/watch.html')
+def watch():
+    return flask.render_template('watch.html')
+
+
 if __name__ == '__main__':
     main()
