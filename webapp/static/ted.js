33b3beeede2b6f002b19b9af4961e6ae3aa1f147
Soren DeHaan <dehaans@carleton.edu>
2021-11-15 21:58:08 -0600

    blinky

--- webapp/static/ted.js
+++ webapp/static/ted.js
@@ -7,12 +7,12 @@ window.onload = initialize;
 
 function initialize() {
     let element = document.getElementById('random_speaker_button');
+    let element2 = document.getElementById('submit');
     if (element) {
         element.onclick = onRandomSpeakerButton;
     }
-    let element2 = document.getElementById('submit');
-    if(element2){
-        element2.onClick = search_speaker;
+    if (element2) {
+        element2.onclick = search_speaker;
     }
 }
 
@@ -47,38 +47,38 @@ function onRandomSpeakerButton() {
 function search_speaker() {
     let input = document.getElementById('speaker_search').value;
     let url = getAPIBaseURL() + '/search_videos/?search=' + input;
-    fetch(url, {method: 'get'})
-    .then((response) => response.json())
-    .then(function(speakers) {
-        let newHTML = '';
-        for(let i = 0; i < speakers.length; i++){
-            let speaker = speakers[i];
-            newHTML += '<div class="video">'
-                     + '<div class="imageWrapper">'
-                     + '<a href="video.html"><img'
-                     + 'src="' + speaker["image"] + '"'
-                     + 'class="" title="" alt="video"></a>'
-                     + '</div>'
-                     + '<div class="videoSidebar">'
-                     + '<div class="title">'
-                     + '<a href="video.html" title="" onclick="">'
-                     + speaker["name"] + '</a>'
-                     + '</div>'
-                     + '<div class="descriptionPadding">'
-                     + '<div class="description">'
-                     + speaker["description"]
-                     + '</div></div>' 
-                     + '<div class="metadata">'
-                     + '<div class="views">Views: 83,190</div>'
-                     + '<div class="runtime">04:11</div>'
-                     + '</div></div></div>';  
-        }
-        let videos = document.getElementById('video_box');  
-        if(videos){
-            videos.innerHTML = video_box;
-        }
-    })
-    .catch(function(error){
-        console.log(error);
-    });
+    fetch(url, { method: 'get' })
+        .then((response) => response.json())
+        .then(function (speakers) {
+            let newHTML = '';
+            for (let i = 0; i < speakers.length; i++) {
+                let speaker = speakers[i];
+                newHTML += '<div class="video">'
+                    + '<div class="imageWrapper">'
+                    + '<a href="video.html"><img'
+                    + 'src="' + speaker["image"] + '"'
+                    + 'class="" title="" alt="video"></a>'
+                    + '</div>'
+                    + '<div class="videoSidebar">'
+                    + '<div class="title">'
+                    + '<a href="video.html" title="" onclick="">'
+                    + speaker["name"] + '</a>'
+                    + '</div>'
+                    + '<div class="descriptionPadding">'
+                    + '<div class="description">'
+                    + speaker["description"]
+                    + '</div></div>'
+                    + '<div class="metadata">'
+                    + '<div class="views">Views: 83,190</div>'
+                    + '<div class="runtime">04:11</div>'
+                    + '</div></div></div>';
+            }
+            let videos = document.getElementById('video_box');
+            if (videos) {
+                videos.innerHTML = newHTML;
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
